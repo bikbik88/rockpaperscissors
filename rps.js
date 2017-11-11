29293a98b72c86a4b908279b256c3a76bfcc8545
bikbik88 <32017633+bikbik88@users.noreply.github.com>
2017-11-11 12:33:20 -0500

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
-//code goes here
+ function rps(p1, p2) {
+    if (p1 === p2) {
+        return "Tie game";
+    } else if (p1 === "rock" && p2 === "paper") {
+        return "p2 wins";
+    }
+    else if (p1==="paper" && p2==="scissors"){
+        return "p2 wins";
+    } else if (p1==="scissors" && p2==="rock") {
+        return "p2 wins";
+    }else if (p1 === "paper" && p2 === "rock") {
+        return "p1 wins";
+    }
+    else if (p1==="scissors" && p2==="paper"){
+        return "p1 wins";
+    } else if (p1==="rock" && p2==="scissors") {
+        return "p1 wins";
+    }
+
+}
